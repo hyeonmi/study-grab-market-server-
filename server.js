75a2b33d1d3b862ce2d769879a7e234e1a9303d1
hyeonmi <hyeonmi.kim@sk.com>
2023-10-24 14:44:28 +0900

    init

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(express.json()); // json 형식의 데이터를 사용
+app.use(cors());
+
+app.get("/products", (req, res) => {
+    res.send({
+        "products": [
+            {
+                "id": 1,
+                "name": "농구공",
+                "price": 10000,
+                "seller": "Jordan",
+                "imageUrl": "images/products/basketball1.jpeg"
+            },
+            {
+                "id": 2,
+                "name": "축구공",
+                "price": 50000,
+                "seller": "Jordan",
+                "imageUrl": "images/products/soccerball1.jpg"
+            },
+            {
+                "id": 3,
+                "name": "키보드",
+                "price": 15000,
+                "seller": "Grab",
+                "imageUrl": "images/products/keyboard1.jpg"
+            }
+        ]
+    });
+});
+
+app.get("/product/:id", (req, res) => {
+    const { id } = req.params;
+    res.send({
+        "id": id,
+        "name": "농구공",
+        "price": 10000,
+        "seller": "Jordan",
+        "imageUrl": "images/products/basketball1.jpeg",
+        "description": "농구공입니다."
+    });
+})
+
+app.post("/products", (req, res) => {
+    const body = req.body;
+    res.send({
+        body
+    });
+});
+
+app.listen(port, () => {
+    console.log('서버가 동작중입니다.')
+});
+
+
